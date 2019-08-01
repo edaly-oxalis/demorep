f96221cc6e254f8acf301a7a5594998186ac221d
edaly-oxalis <edaly@oxalis.io>
2019-08-01 15:27:48 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const fs = require ('fs')
+
+fs.writeFileSync('notes.txt', 'Some text.\n')
+
+try {
+    fs.appendFileSync('notes.txt', 'Some more lines of text.');
+    console.log('The "data to append" was appended to file!');
+} catch (err) {
+    /* Handle the error */
+}
